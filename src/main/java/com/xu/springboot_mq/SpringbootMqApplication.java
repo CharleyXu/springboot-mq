422c0291b7297eacc7b8658cb723309cccfed55a
CharleyXu <xucharlie1993@gmail.com>
2017-12-01 19:34:19 +0800

    rabbit_config

--- src/main/java/com/xu/springboot_mq/SpringbootMqApplication.java
+++ src/main/java/com/xu/springboot_mq/SpringbootMqApplication.java
@@ -1,9 +1,12 @@
 package com.xu.springboot_mq;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootMqApplication {
 
 	public static void main(String[] args) {
